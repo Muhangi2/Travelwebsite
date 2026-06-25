ac5eb9d419b552ba92c8ee9cccb0be4af4478d1f
eliodamutiba <eliodamutiba@gmail.com>
2026-06-26 00:12:52 +0300

    lastest

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 export const site = {
   name: 'Still Wild Safaris',
   tagline: 'Bespoke luxury safaris in Uganda, Rwanda and Kenya',
-  email: 'info@masterpolosafaris.com',
+  email: 'info@stillwildsafaris.com',
   phone: '+256 769 797 796',
   phoneAlt: '+256 705 527 599',
   whatsapp: '+256 740 305 487',
